017af662fae81c7af8d395561ecb2724773d2d66
sambilokonga <abebawbegna@gmail.com>
2022-09-08 08:05:48 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 /**
+ * main – Entry point 
  *
- * * main – Entry point 
- *
- * *
- *
- * * Return:  Always 0 (success)
- *
- * */
+ * Return:  Always 0 (success)
+ */
 
 int main(void)
 
