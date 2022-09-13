987769dd410780604ec03b742eb22c267a4ba4b4
sambilokonga <abebawbegna@gmail.com>
2022-09-13 03:33:30 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,15 @@
  */
 int main(void)
 {
-	char *sh = "School";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
