d81d5243a45d3f136b2fb6dfd314bd31c8d0eadd
sambilokonga <abebawbegna@gmail.com>
2022-09-19 05:23:11 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string, in reverse
+ * fcounter is to first count to end, n is to count back
+ * @s: str input
+ * Return: string in reverse
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+		i += 1;
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
+
