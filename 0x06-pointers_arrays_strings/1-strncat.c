743560a7cad488cd2df1aeeb638b15497e12c587
sambilokonga <abebawbegna@gmail.com>
2022-09-22 02:17:54 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,6 +4,7 @@
  * _strncat - concatenates two strings
  * @dest: pointer to the destination string
  * @src: pointer to the source string
+ * @n: number of bytes to print
  * Return: pointer to the resulting string dest
  */
 
